cf23ab15aa0d7cbafa95e14b46f23a257d5701d9
Ilham9675 <ilhamakbar9675@gmail.com>
2024-05-10 18:21:58 +0700

    up

---
+++ lib/models/item.dart
@@ -0,0 +1,25 @@
+class Item {
+  final String title;
+  final String description;
+  final int price;
+  final String imgName;
+  final int id;
+
+  Item({
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imgName,
+    required this.id,
+  });
+
+  factory Item.fromJson(Map<String, dynamic> json) {
+    return Item(
+      title: json['title'],
+      description: json['description'],
+      price: json['price'],
+      imgName: json['img_name'],
+      id: json['id'],
+    );
+  }
+}
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,8 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+import 'package:http/http.dart' as http;
+import 'package:kuis_webapi/models/item.dart';
+import 'dart:convert';
+
 import 'login.dart';
 import 'bottomNav.dart'; // Pastikan untuk mengimpor file yang berisi BottomNav
+
+
+
+
+Future<List<Item>> fetchItems() async {
+  final String? accessToken = Hive.box("login").get('accessToken');
+
+  if (accessToken == null) {
+    throw Exception('No access token found');
+  }
+
+  final response = await http.get(
+    Uri.parse('http://146.190.109.66:8000/items/?skip=0&limit=100'),
+    headers: {
+      'Authorization': 'Bearer $accessToken',
+    },
+  );
+
+  if (response.statusCode == 200) {
+    print('berhasil');
+    List jsonResponse = json.decode(response.body);
+    return jsonResponse.map((item) => Item.fromJson(item)).toList();
+  } else {
+    print('test');
+    throw Exception('Failed to load items');
+  }
+}
+
 
 class Home extends StatelessWidget {
   Home({super.key});
@@ -45,19 +77,26 @@ class Home extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.primary,
       bottomNavigationBar: BottomNavigasiBar(inputan: 0),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Welcome 🎉",
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'home',
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-          ],
+        child: FutureBuilder<List<Item>>(
+          future: fetchItems(),
+          builder: (BuildContext context, AsyncSnapshot<List<Item>> snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(snapshot.data![index].title),
+                    subtitle: Text(snapshot.data![index].description),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
+
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
         ),
       ),
     );
