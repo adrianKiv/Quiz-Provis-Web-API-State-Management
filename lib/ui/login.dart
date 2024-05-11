93aee7de41ba3c9a7cc4ab70d72fbc6090e0057d
adrianKiv <adrianmulianto@gmail.com>
2024-05-11 11:12:48 +0700

    donee

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -94,7 +94,7 @@ class _LoginState extends State<Login> {
             children: [
               const SizedBox(height: 150),
               Text(
-                "Welcome back",
+                "Kelompok: 3",
                 style: Theme.of(context).textTheme.headlineLarge,
               ),
               const SizedBox(height: 10),
